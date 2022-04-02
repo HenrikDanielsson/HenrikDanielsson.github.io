9c71ce98e1be1baa15dad73128a81224f54ed45e
Henrik Danielsson <henrik.danielsson@me.com>
2022-04-02 02:24:12 +0200

    Ändrat css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>14<span className="reverse">6</span><span className="small">.S<span className="rot45" char="E">E</span></span></h1>
+        <h1>14<span className="reverse">6</span><span className="small">.S<span className="rotate">E</span></span></h1>
         <p>A placeholder for future projects.</p>
         <img src='gfx/instagram-16.png' onClick={() => {
       window.open("http://instagram.com/146_north", "_blank");
